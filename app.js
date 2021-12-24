8c32856cd1c3e110c24a08712a845cd152746a0f
veerendranath0312 <veerendranath0312@gmail.com>
2021-12-25 00:34:48 +0530

    Added functionality

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+// Select elements
+const form = document.querySelector('form')
+const task = document.getElementById('task')
+const dayAndTime = document.getElementById('day-and-time')
+const taskList = document.querySelector('.task-list')
 
+taskList.innerHTML = ''
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  // generate the listItem html
+  const listItemHtml = `
+    <div class="list-item">
+      <div class="left">
+        <h4>${task.value}</h4>
+        <p>${dayAndTime.value}</p>
+      </div>
+      <div class="right">
+        <button><ion-icon name="trash-outline"></ion-icon></button>
+      </div>
+    </div>
+  `
+
+  // Adding tas to the taskList
+  if (task.value && dayAndTime.value) {
+    taskList.insertAdjacentHTML('beforeend', listItemHtml)
+  }
+
+  // Clear the input fields
+  task.value = ''
+  dayAndTime.value = ''
+})
+
+// removing the task from the taskList
+taskList.addEventListener('click', e => {
+  e.target.closest('.list-item').remove()
+})
